9997badbb798b71b472088f485e95b663385e8d1
gustavo-hillesheim <guhillesheim@gmail.com>
2021-02-08 19:43:56 -0300

    Adicionado frontend

---
+++ bank-api/src/dto/pix-key.dto.ts
@@ -0,0 +1,12 @@
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
+import { PixKeyKind } from 'src/models/pix-key.model';
+
+export class PixKeyDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly key: string;
+
+  @IsString()
+  @IsIn(Object.values(PixKeyKind))
+  readonly kind: PixKeyKind;
+}
---
+++ bank-api/src/fixtures/fixtures.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    model: 'BankAccount',
+    fields: {
+      id: '7c76f52b-9cf9-4237-b315-461ad9f700f9',
+      account_number: '1111',
+      owner_name: 'User BBX 1',
+    },
+  },
+  {
+    model: 'BankAccount',
+    fields: {
+      id: '6e75ddd4-8890-4d4a-8b7c-b0168c1893cd',
+      account_number: '2222',
+      owner_name: 'User BBX 2',
+    },
+  },
+];
